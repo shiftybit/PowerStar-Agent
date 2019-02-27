e06ed6b33ea2c53467a9533f4470d643395a70b2
Devon Dieffenbach <devon.dieffenbach@gmail.com>
2019-02-27 10:57:24 -0500

    Making the dream work, with a frame work.

---
+++ PowerStar-Agent/Agent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace PowerStar_Agent
+{
+	static class Agent
+	{
+		private static PowerShell powershell;
+
+		static Action<string> WriteLine;
+
+		public static void Initialize(Action<String> writeLine = null)
+		{
+			WriteLine = writeLine;
+			InitialSessionState state = InitialSessionState.CreateDefault();
+			SessionStateVariableEntry agentEntry = new SessionStateVariableEntry("Agent", typeof(Agent), "The Agent");
+			state.Variables.Add(agentEntry);
+			powershell = PowerShell.Create(state);
+		}
+
+		public static string RunString(string text)
+		{
+			powershell.AddScript(text);
+			string returnString = "";
+			foreach (dynamic item in powershell.Invoke().ToList())
+			{
+				returnString += item.ToString();
+			}
+			return returnString;
+		}
+	}
+}
--- PowerStar-Agent/Form1.cs
+++ PowerStar-Agent/Form1.cs
@@ -12,9 +12,49 @@ namespace PowerStar_Agent
 {
 	public partial class Form1 : Form
 	{
+		delegate void WriteLineDelegate(string text);
 		public Form1()
 		{
 			InitializeComponent();
+		}
+
+		public void WriteLine(string text)
+		{
+			if (!textBox1.InvokeRequired)
+				textBox1.Text += text + "\r\n";
+			else
+			{
+				WriteLineDelegate d = new WriteLineDelegate(WriteLine);
+				this.Invoke(d, new object[] { text });
+			}
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Agent.Initialize();
+		}
+
+		private void TextBox1_TextChanged(object sender, EventArgs e)
+		{
+
+			textBox1.SelectionStart = textBox1.Text.Length;
+			textBox1.ScrollToCaret();
+		}
+		private void RunCurrentText()
+		{
+			var script = textBox2.Text.ToString();
+			textBox2.Clear();
+			WriteLine("PS> " + script);
+			WriteLine(Agent.RunString(script));
+		}
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			RunCurrentText();
+		}
+
+		private void TextBox2_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+				RunCurrentText();
 		}
 	}
 }
